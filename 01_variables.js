b43d7d4ff84c24731f546b0d19a20d99b5290344
Bibhuti Bhusan Ghosh <142000199+bhusan404@users.noreply.github.com>
2023-10-22 10:11:42 +0530

    Update and rename let_const_var.js to 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "bibhu111@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+//accountId = 2
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in blocck scope and functional scope
+
+Only use const and var. Dont keep the prefix blank
+*/
